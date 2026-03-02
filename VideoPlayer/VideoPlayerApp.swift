a16249467cf60a2cf17229da8d027beb19bb6e69
Anticlub <cristofdzfdz@gmail.com>
2026-03-02 11:51:08 +0100

    Initial Commit

---
+++ VideoPlayer/VideoPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoPlayerApp.swift
+//  VideoPlayer
+//
+//  Created by cristofer fernandez on 2/3/26.
+//
+
+import SwiftUI
+
+@main
+struct VideoPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
