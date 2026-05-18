7115e5dac9b15e0227bf26a9bb02e19011da87ed
Anticlub <cristofdzfdz@gmail.com>
2026-05-18 16:05:30 +0200

    test(PlayerViewModel): add unit tests for selectChannel, stop and playPause

--- VideoPlayerTests/VideoPlayerTests.swift
+++ VideoPlayerTests/VideoPlayerTests.swift
@@ -5,32 +5,32 @@
 //  Created by cristofer fernandez on 2/3/26.
 //
 
-import XCTest
+import Testing
 @testable import VideoPlayer
+import Foundation
 
-final class VideoPlayerTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
+@Test @MainActor func selectChannel_shouldCallLoad() throws {
+    let mock = MockPlayerService()
+    let player = PlayerViewModel(playerService: mock)
+    let url = Foundation.URL(string: "https://test.com")
+    let channel = Channel(name: "Test", url: url!)
+    player.selectChannel(channel)
+    #expect(mock.loadWasCalled == true)
+    
 }
+
+@Test @MainActor func stop_shouldCallStop() throws {
+    let mock = MockPlayerService()
+    let player = PlayerViewModel(playerService: mock)
+    player.stop()
+    #expect(mock.stopWasCalled == true)
+}
+
+@Test @MainActor func playPause_shouldCallTogglePlayPause() throws {
+    let mock = MockPlayerService()
+    let player = PlayerViewModel(playerService: mock)
+    player.playPause()
+    #expect(mock.togglePlayPauseWasCalled == true)
+}
+
+// TODO: testear nextChannel y previousChannel una vez que se refactorice PlayerViewModel para permitir inyectar canales
