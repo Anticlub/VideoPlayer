281324c47e2274f709ec82f988fc1ea4c5f0668c
Anticlub <cristofdzfdz@gmail.com>
2026-05-22 14:27:53 +0200

    feat(DRMErrorTests): add certificateFetchFailed status code test

---
+++ VideoPlayerTests/DRMErrorTest.swift
@@ -0,0 +1,20 @@
+//
+//  Untitled.swift
+//  VideoPlayer
+//
+//  Created by cristofer fernandez on 22/05/2026.
+//
+
+import Testing
+@testable import VideoPlayer
+import Foundation
+
+@Test @MainActor func drmError_certificateFetchFailed_shouldContainStatusCode() {
+    let error = DRMManager.DRMError.certificateFetchFailed(statusCode: 404)
+    
+    if case .certificateFetchFailed(let statusCode) = error {
+        #expect(statusCode == 404)
+    } else {
+        Issue.record("Expected certificateFetchFailed")
+    }
+}
